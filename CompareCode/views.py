073e2a4c11cf1526f448886e127ef9c1b99708a0
vaapp <vaapp@microsoft.com>
2019-10-10 10:15:25 +0530

    add comment 2

--- CompareCode/views.py
+++ CompareCode/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.template import RequestContext
-
+#t2
 
 def showCompare(request):
     context = {'latest_question_list': 'something'}
